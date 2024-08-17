ae4a4fe6355a9d468ce3df593907b904a88bc3ac
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-08-17 18:41:43 -0500

    Updated to Axios api requests

--- src/Nav.js
+++ src/Nav.js
@@ -13,12 +13,12 @@ const Nav = ({ search, setSearch }) => {
 					value={search}
 					onChange={(e) => setSearch(e.target.value)}
 				/>
-				<ul>
-					<li><Link to="/">Home</Link></li>
-					<li><Link to="/post">Post</Link></li>
-					<li><Link to="/about">About</Link></li>
-				</ul>
 			</form>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/post">Post</Link></li>
+				<li><Link to="/about">About</Link></li>
+			</ul>
 		</nav>
 	)
 }
