0c3a874bc451d29fbdfa5f1335008fbb0fce7f0f
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-08-18 09:01:29 -0500

    Added Edit and Delete functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './Footer';
 import Home from './Home';
 import NewPost from './NewPost';
 import PostPage from './PostPage';
+import EditPost from './EditPost';
 import About from './About';
 import Missing from './Missing';
 import { Router, Route, Routes, useNavigate } from 'react-router-dom';
@@ -18,6 +19,8 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -65,10 +68,29 @@ function App() {
     }
   }
 
-  const handleDelete = (id) => {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    navigate('/');
+  const handleEdit =   async (id) => {
+    const datetime = format(new Date(), 'MMMM dd, yyy pp');
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
+      setEditTitle('');
+      setEditBody('');
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: $ {err.message}`);
+    }
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter(post => post.id !== id);
+      setPosts(postsList);
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: $ {err.message}`);
+    }
   }
 
   return (
@@ -85,6 +107,14 @@ function App() {
           setPostTitle={setPostTitle} 
           postBody={postBody} 
           setPostBody={setPostBody} />} 
+        />
+        <Route path="edit/:id" element={<EditPost 
+          posts={posts} 
+          handleEdit={handleEdit} 
+          editTitle={editTitle} 
+          setEditTitle={setEditTitle} 
+          editBody={editBody} 
+          setEditBody={setEditBody} />} 
         />
         <Route path="post/:id" element={<PostPage 
           posts={posts} 
