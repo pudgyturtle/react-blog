32391801343662cd11826582dac449c9884d02ec
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-08-20 11:38:49 -0500

    Refactored to use createContext

--- src/App.js
+++ src/App.js
@@ -7,115 +7,28 @@ import PostPage from './PostPage';
 import EditPost from './EditPost';
 import About from './About';
 import Missing from './Missing';
-import { Router, Route, Routes, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { format } from 'date-fns';
-import api from './api/posts';
-import useWindowSize from './hooks/useWindowSize';
-import useAxiosFetch from './hooks/useAxiosFetch';
+import { Router, Route, Routes } from 'react-router-dom';
+import { DataProvider } from './context/DataContext';
 
 function App() {
-  const [posts, setPosts] = useState([])
-  const [search, setSearch] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
-  const [editTitle, setEditTitle] = useState('');
-  const [editBody, setEditBody] = useState('');
-  const navigate = useNavigate();
-  const { width } = useWindowSize();
-
-  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
-  
-  useEffect(() => {
-    setPosts(data);
-  }, [data])
-
-  useEffect(() => {
-    const filteredResults = posts.filter(post =>
-    ((post.body).toLowerCase()).includes(search.toLowerCase())
-    || ((post.title).toLowerCase()).includes(search.toLowerCase()));
-
-    setSearchResults(filteredResults.reverse());
-  },[posts, search])
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
-    const datetime = format(new Date(), 'MMMM dd, yyy pp');
-    const newPost = { id, title: postTitle, datetime, body: postBody };
-    try {
-      const response = await api.post('/posts', newPost);
-      const allPosts = [ ...posts, response.data ];
-      setPosts(allPosts);
-      setPostTitle('');
-      setPostBody('');
-      navigate('/');
-    } catch (err) {
-      console.log(`Error: $ {err.message}`);
-    }
-  }
-
-  const handleEdit =   async (id) => {
-    const datetime = format(new Date(), 'MMMM dd, yyy pp');
-    const updatedPost = { id, title: editTitle, datetime, body: editBody };
-    try {
-      const response = await api.put(`/posts/${id}`, updatedPost);
-      setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
-      setEditTitle('');
-      setEditBody('');
-      navigate('/');
-    } catch (err) {
-      console.log(`Error: $ {err.message}`);
-    }
-  }
-
-  const handleDelete = async (id) => {
-    try {
-      await api.delete(`/posts/${id}`);
-      const postsList = posts.filter(post => post.id !== id);
-      setPosts(postsList);
-      navigate('/');
-    } catch (err) {
-      console.log(`Error: $ {err.message}`);
-    }
-  }
 
   return (
     <div className="App">
-      <Header title="React JS Blog" width={width} />
-      <Nav search={search} setSearch={setSearch} />
-      <Routes>
-        <Route path="/" element={<Home 
-          posts={searchResults}
-          fetchError={fetchError}
-          isLoading={isLoading} />} 
-        />
-        <Route path="post" element={<NewPost 
-          handleSubmit={handleSubmit} 
-          postTitle={postTitle} 
-          setPostTitle={setPostTitle} 
-          postBody={postBody} 
-          setPostBody={setPostBody} />} 
-        />
-        <Route path="edit/:id" element={<EditPost 
-          posts={posts} 
-          handleEdit={handleEdit} 
-          editTitle={editTitle} 
-          setEditTitle={setEditTitle} 
-          editBody={editBody} 
-          setEditBody={setEditBody} />} 
-        />
-        <Route path="post/:id" element={<PostPage 
-          posts={posts} 
-          handleDelete={handleDelete}/>} 
-        />
-        <Route path="about" element={<About />} 
-        />
-        <Route path="*" element={<Missing />} 
-        />
-      </Routes>
-      <Footer />
+      <DataProvider>
+        <Header title="React JS Blog" />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="post" element={<NewPost />} />
+          <Route path="edit/:id" element={<EditPost />} />
+          <Route path="post/:id" element={<PostPage />} />
+          <Route path="about" element={<About />} 
+          />
+          <Route path="*" element={<Missing />} 
+          />
+        </Routes>
+        <Footer />
+      </DataProvider>
     </div>
   );
 }
--- src/EditPost.js
+++ src/EditPost.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
+import DataContext from "./context/DataContext";
 
+const EditPost = () => {
+    const { posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle } = useContext(DataContext);
 
-const EditPost = ({
-    posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle
-}) => {
     const { id } = useParams();
     const post = posts.find(post => (post.id).toString() === id);
 
