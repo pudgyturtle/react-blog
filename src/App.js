0f775dcbe0b505455fd3410a941c4abe7731b2b9
pudgyturtle <12068664+pudgyturtle@users.noreply.github.com>
2024-08-19 20:17:25 -0500

    Updated to Axios custom hook

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import api from './api/posts';
 import useWindowSize from './hooks/useWindowSize';
+import useAxiosFetch from './hooks/useAxiosFetch';
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -24,26 +25,12 @@ function App() {
   const navigate = useNavigate();
   const { width } = useWindowSize();
 
-  useEffect(() => {
-    const fetchPosts = async () =>  {
-      try {
-        const response = await api.get('/posts');
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          // Not in 200 response range
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: $(err.message)`);
-        }
-      }
-    }
-
-    fetchPosts();
-  }, [])
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
   
+  useEffect(() => {
+    setPosts(data);
+  }, [data])
+
   useEffect(() => {
     const filteredResults = posts.filter(post =>
     ((post.body).toLowerCase()).includes(search.toLowerCase())
@@ -100,7 +87,9 @@ function App() {
       <Nav search={search} setSearch={setSearch} />
       <Routes>
         <Route path="/" element={<Home 
-          posts={searchResults} />} 
+          posts={searchResults}
+          fetchError={fetchError}
+          isLoading={isLoading} />} 
         />
         <Route path="post" element={<NewPost 
           handleSubmit={handleSubmit} 
